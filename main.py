b4275bc0ce4a5631430d36a56bb07836263ad9d3
s <s>
2022-07-01 15:26:41 +0500

    get ip from 2ip.ru

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-l = [1,2,3,4,5]
+import requests
+from bs4 import BeautifulSoup
 
-for i in range(1,2):
-  print(l[-1])
+
+headers = {
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+}
+
+def get_location(url):
+  response = requests.get(url=url, headers=headers)
+  soup = BeautifulSoup(response.text, 'lxml')
+
+  ip = soup.find('div', class_='ip').text.strip()
+  print(ip)
+
+def main():
+  get_location(url='https://2ip.ru')
+
+
+if __name__ == '__main__':
+  main()
